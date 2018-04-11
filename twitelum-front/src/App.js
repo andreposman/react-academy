5e5eaf66b51d64521cfb2344a444f7d7657c6db0
daninaomi <daninaomi93@gmail.com>
2018-04-10 21:58:18 -0300

    Cap. 7

--- twitelum-front/src/App.js
+++ twitelum-front/src/App.js
@@ -6,39 +6,77 @@ import Widget from './components/Widget'
 import TrendsArea from './components/TrendsArea'
 import Tweet from './components/Tweet'
 
+
+
 class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Cabecalho>
-            <NavMenu usuario="@andreposman" />
-        </Cabecalho>
-        <div className="container">
-            <Dashboard>
-                <Widget>
-                    <form className="novoTweet">
-                        <div className="novoTweet__editorArea">
-                            <span className="novoTweet__status">0/140</span>
-                            <textarea className="novoTweet__editor" placeholder="O que está acontecendo?"></textarea>
-                        </div>
-                        <button type="submit" className="novoTweet__envia">Tweetar</button>
-                    </form>
-                </Widget>
-                <Widget>
-                    <TrendsArea />
-                </Widget>
-            </Dashboard>
-            <Dashboard posicao="centro">
-                <Widget>
-                    <div className="tweetsArea">
-                        <Tweet />
-                    </div>
-                </Widget>
-            </Dashboard>
-        </div>
-      </Fragment>
-    );
-  }
+    constructor() {
+        super()
+
+        this.state = {
+            novoTweet: '',
+            tweets: []
+        }
+        //bind
+        this.adicionaTweet = this.adicionaTweet.bind(this)
+    }
+
+    adicionaTweet(e) {
+        e.preventDefault();
+        const novoTweet = this.state.novoTweet
+
+        this.setState({
+            tweets: [novoTweet, ...this.state.tweets]
+        })
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Cabecalho>
+                    <NavMenu usuario="@andreposman" />
+                </Cabecalho>
+                <div className="container">
+                    <Dashboard>
+                        <Widget>
+                            <form className="novoTweet" onSubmit={this.adicionaTweet}>
+                                <div className="novoTweet__editorArea">
+                                    <span className={`novoTweet__status ${
+                                        this.state.novoTweet.length > 140
+                                            ? 'novoTweet__status--invalido' : ''
+                                        }`}>
+                                        {this.state.novoTweet.length}/140
+                                        </span>
+                                    <textarea
+                                        value={this.state.novoTweet}
+                                        onChange={(event) => { this.setState({ novoTweet: event.target.value }) }}
+                                        className="novoTweet__editor" placeholder="O que está acontecendo?"></textarea>
+                                </div>
+                                <button type="submit" className="novoTweet__envia"
+                                    disabled={
+                                        this.state.novoTweet.length > 140
+                                            ? true : false
+                                    }>Tweetar</button>
+                            </form>
+                        </Widget>
+                        <Widget>
+                            <TrendsArea />
+                        </Widget>
+                    </Dashboard>
+                    <Dashboard posicao="centro">
+                        <Widget>
+                            <div className="tweetsArea">
+                                {this.state.tweets.map((tweetInfo, index) =>
+                                    <Tweet
+                                        texto={tweetInfo}
+                                        key={tweetInfo + index} />
+                                )}
+                            </div>
+                        </Widget>
+                    </Dashboard>
+                </div>
+            </Fragment >
+        );
+    }
 }
 
 export default App;
